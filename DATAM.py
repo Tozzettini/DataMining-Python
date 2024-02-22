8f2aad5989c54ecbf6f091485549592f39482563
Tozzettini <lziche@gmail.com>
2024-02-22 09:39:01 +0100

    Initial week 1 python basics review

---
+++ DATAM.py
@@ -0,0 +1,9 @@
+print('Hello world')  # This will print the text string 'Hello world'
+
+a = 9
+b = 'Some string'
+c = 1.23
+d = True
+
+print(type(a), type(b), type(c)) # Prints "<class 'int'> <class 'str'> <class 'float'>".
+
