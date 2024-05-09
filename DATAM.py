9e606b48507516b78835715c9c157d1dc587fb8f
Tozzettini <lziche@gmail.com>
2024-05-09 09:49:14 +0200

    push

--- DATAM.py
+++
@@ -1,9 +0,0 @@
-print('Hello world')  # This will print the text string 'Hello world'
-
-a = 9
-b = 'Some string'
-c = 1.23
-d = True
-
-print(type(a), type(b), type(c)) # Prints "<class 'int'> <class 'str'> <class 'float'>".
-
